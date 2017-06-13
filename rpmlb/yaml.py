219734a8ffd5b8de89f198ce3e6cd84aa2360783
Jun Aruga <jaruga@redhat.com>
2017-06-13 17:04:47 +0200

    Nit picking for yaml.py
    * Move import lines to the beginning of the file.
    * Change Yaml class to functions.

--- rpmlb/yaml.py
+++ rpmlb/yaml.py
@@ -1,30 +1,29 @@
 import logging
 import os
 import yaml
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+try:
+    from yaml import CDumper as Dumper
+except ImportError:
+    from yaml import Dumper
 
 LOG = logging.getLogger(__name__)
 
 
-class Yaml:
-    """A class to manage YAML data."""
+def load(file_path):
+    try:
+        obj = None
+        with open(file_path, 'r') as stream:
+            obj = yaml.load(stream, Loader=Loader)
+        return obj
+    except FileNotFoundError as e:
+        LOG.error('File not found: %s at %s', file_path, os.getcwd())
+        raise e
 
-    def __init__(self, file_path):
-        try:
-            from yaml import CLoader as Loader
-        except ImportError:
-            from yaml import Loader
-        try:
-            with open(file_path, 'r') as stream:
-                self.content = yaml.load(stream, Loader=Loader)
-        except FileNotFoundError as e:
-            LOG.error('File not found: %s at %s', file_path, os.getcwd())
-            raise e
-
-    def dump(self):
-        try:
-            from yaml import CDumper as Dumper
-        except ImportError:
-            from yaml import Dumper
-        output = yaml.dump(self.content, Dumper=Dumper,
+    def dump(obj):
+        output = yaml.dump(obj, Dumper=Dumper,
                            default_flow_style=False)
         print(output)
